f6a890625109fd236375794d31c6f1025913f604
João Nunes <66601995+nunesdotpy@users.noreply.github.com>
2023-03-04 01:16:07 -0300

    bot 1.0

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+from key import token
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+TOKEN = token.get('TOKEN')
+
+@client.event
+async def on_ready():
+    print(f'{client.user} está online!')
+
+@client.event
+async def on_message(message):
+
+    conteudo = message.content
+    l_conteudo = conteudo.lower()
+
+    if message.author == client.user:
+        return
+
+    if l_conteudo.startswith('oi'):
+        await message.channel.send(f'Oi {message.author}, tudo bem?')
+
+client.run(TOKEN)
